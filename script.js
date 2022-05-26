e1b9c88f4e2cc3ca687b0e132e044748fb786b88
CarlosMosqueira <63421328+CarlosMosqueira@users.noreply.github.com>
2022-05-25 22:14:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+function calcular(){
+
+    var altura = document.getElementById("altura").value / 100;
+    var peso = document.getElementById("peso").value;
+
+    var imc = peso/(altura) **2;
+
+    if (imc < 18.5) {
+        window.alert("Você está magro")
+      } else if (imc < 24.9) {
+        window.alert("Você está normal")
+      } else if (imc < 29.9) {
+        window.alert("Você está com sobrepeso")
+      } else if (imc < 39.9) {
+        window.alert("Você está com obesidade")
+      } else if (imc > 39.9) {
+        window.alert("Você está com obesidade mórbida")
+
+    } else {
+        window.alert("Insira um valor nos campos")
+    }
+}
+
+function limpar(){
+    peso.value = "";
+   altura.value = "";
+}
